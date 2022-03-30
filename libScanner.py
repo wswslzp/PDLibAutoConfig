26d9795a7ab1215272f55f5b744853b3ac8b20d4
Zhengpeng Liao <wswslzp@outlook.com>
2022-03-30 11:23:10 +0800

    update

--- libScanner.py
+++ libScanner.py
@@ -80,8 +80,8 @@ class TimingLib(object):
     
     def scanForPVT(self, content):
         p_pat = re.compile(r"\s*operating_conditions\s*\(\s*(\w+)\s*\)")
-        nt_pat = re.compile(r"\s*nom_temperature\s*:\s*(-?[1-9]\d*|0)\s*;")
-        nv_pat = re.compile(r"\s*nom_voltage\s*:\s*([1-9]\d*\.\d*|0\.\d*[1-9]\d*)\s*;")
+        nt_pat = re.compile(r"\s*(nom_)?temperature\s*:\s*(-?[1-9]\d*|0)\s*;")
+        nv_pat = re.compile(r"\s*(nom_)?voltage\s*:\s*([1-9]\d*\.\d*|0\.\d*[1-9]\d*)\s*;")
         matched = 0
         for line in content:
             line = toStr(line)
